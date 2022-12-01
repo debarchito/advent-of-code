cc27b08f7ecbe1f2575d4f78f16b799b3b7fd814
debarchito <dev.debarchito.nath@protonmail.com>
2022-12-01 21:50:41 +0530

    Day 1 (2022)

---
+++ 2022/Day 1/main.jl
@@ -0,0 +1,22 @@
+module Day1
+  open("Day1/input.txt") do f
+    l = [i for i in split(read(f, String), "\n")]
+    global c = 0
+    ls = []
+
+    for i in l
+      if !isempty(i)
+        global c += parse(Int64, i)
+        continue
+      end
+
+      push!(ls, c)
+      global c = 0
+    end
+
+    sort!(ls)
+
+    println("Part 1: ", ls[end])
+    println("Part 2: ", +(ls[end], ls[end - 1], ls[end - 2]))
+  end
+end
